59e258c5814314776575cd7b1660c425efe7f00a
brad_xps <bradley.lindblad@gmail.com>
2019-08-25 13:59:21 -0500

    FInished getParams.R functions
    - finished getParams fns
    - did test build, which passed

--- R/getParams.R
+++ R/getParams.R
@@ -4,13 +4,23 @@ library(jsonlite)
 
 
 
-program, sector, group, commodity,
-category, data_item, domain, geographic_level,
-region, ag_district, state, county, year
-
-key <- '7CE0AFAD-EF7B-3761-8B8C-6AF474D6EF71'
+# program, sector, group, commodity,
+# category, data_item, domain, geographic_level,
+# region, ag_district, state, county, year
+# 
+# key <- '7CE0AFAD-EF7B-3761-8B8C-6AF474D6EF71'
 
 getAllProgram <- function(key){
+  
+  #' Get all possible values from PROGRAM field
+  #' 
+  #' @param key: your USDA api key
+  #' 
+  #' @examples
+  #' x <- tidyUSDA::getAllProgram(mykey)
+  #' 
+  #'  # View the vector in RSTudio in order to search for terms 
+  #'  View(x)
   
   param = "source_desc"
   
@@ -32,6 +42,16 @@ getAllProgram <- function(key){
 
 getAllSector <- function(key){
   
+  #' Get all possible values from SECTOR field
+  #' 
+  #' @param key: your USDA api key
+  #' 
+  #' @examples
+  #' x <- tidyUSDA::getAllSector(mykey)
+  #' 
+  #'  # View the vector in RSTudio in order to search for terms 
+  #'  View(x)
+  #'  
   param = "sector_desc"
   
   key <- key
@@ -51,6 +71,17 @@ getAllSector <- function(key){
 }
 
 getAllGroup <- function(key){
+  
+  #' Get all possible values from GROUP field
+  #' 
+  #' @param key: your USDA api key
+  #' 
+  #' @examples
+  #' x <- tidyUSDA::getAllGroup(mykey)
+  #' 
+  #'  # View the vector in RSTudio in order to search for terms 
+  #'  View(x)
+  
   
   param = "group_desc"
   
@@ -72,7 +103,17 @@ getAllGroup <- function(key){
 
 getAllCommodity <- function(key){
   
-  param = "group_desc"
+  #' Get all possible values from COMMODITY field
+  #' 
+  #' @param key: your USDA api key
+  #' 
+  #' @examples
+  #' x <- tidyUSDA::getAllCommodity(mykey)
+  #' 
+  #'  # View the vector in RSTudio in order to search for terms 
+  #'  View(x)
+  
+  param = "commodity_desc"
   
   key <- key
   
@@ -91,6 +132,16 @@ getAllCommodity <- function(key){
 }
 
 getAllCategory <- function(key){
+  
+  #' Get all possible values from CATEGORY field
+  #' 
+  #' @param key: your USDA api key
+  #' 
+  #' @examples
+  #' x <- tidyUSDA::getAllCategory(mykey)
+  #' 
+  #'  # View the vector in RSTudio in order to search for terms 
+  #'  View(x)
   
   param = "statisticcat_desc"
   
@@ -112,6 +163,16 @@ getAllCategory <- function(key){
 
 getAllDataItem <- function(key){
   
+  #' Get all possible values from DATA ITEM field
+  #' 
+  #' @param key: your USDA api key
+  #' 
+  #' @examples
+  #' x <- tidyUSDA::getAllDataItem(mykey)
+  #' 
+  #'  # View the vector in RSTudio in order to search for terms 
+  #'  View(x)
+  
   param = "short_desc"
   
   key <- key
@@ -131,6 +192,16 @@ getAllDataItem <- function(key){
 }
 
 getAllDomain <- function(key){
+  
+  #' Get all possible values from DOMAIN field
+  #' 
+  #' @param key: your USDA api key
+  #' 
+  #' @examples
+  #' x <- tidyUSDA::getAllDomain(mykey)
+  #' 
+  #'  # View the vector in RSTudio in order to search for terms 
+  #'  View(x)
   
   param = "domain"
   
@@ -152,6 +223,16 @@ getAllDomain <- function(key){
 
 getAllGeogLevel <- function(key){
   
+  #' Get all possible values from GEOGRAPHIC LEVEL field
+  #' 
+  #' @param key: your USDA api key
+  #' 
+  #' @examples
+  #' x <- tidyUSDA::getAllGeogLevel(mykey)
+  #' 
+  #'  # View the vector in RSTudio in order to search for terms 
+  #'  View(x)
+  
   param = "agg_level_desc"
   
   key <- key
@@ -169,3 +250,65 @@ getAllGeogLevel <- function(key){
   return(vec)
   
 }
+
+getAllState <- function(key){
+  
+  #' Get all possible values from STATE field
+  #' 
+  #' @param key: your USDA api key
+  #' 
+  #' @examples
+  #' x <- tidyUSDA::getAllState(mykey)
+  #' 
+  #'  # View the vector in RSTudio in order to search for terms 
+  #'  View(x)
+  
+  param = "state_name"
+  
+  key <- key
+  
+  key <- paste0(key,'&param=')
+  
+  base <- 'http://quickstats.nass.usda.gov/api/get_param_values/?key='
+  
+  url <- paste0(base, key , param)
+  
+  vec <- jsonlite::fromJSON(url)
+  
+  vec <- unlist(vec)
+  
+  return(vec)
+  
+}
+
+getAllCounty <- function(key){
+  
+  #' Get all possible values from COUNTY field
+  #' 
+  #' @param key: your USDA api key
+  #' 
+  #' @examples
+  #' x <- tidyUSDA::getAllCounty(mykey)
+  #' 
+  #'  # View the vector in RSTudio in order to search for terms 
+  #'  View(x)
+  
+  param = "county_name"
+  
+  key <- key
+  
+  key <- paste0(key,'&param=')
+  
+  base <- 'http://quickstats.nass.usda.gov/api/get_param_values/?key='
+  
+  url <- paste0(base, key , param)
+  
+  vec <- jsonlite::fromJSON(url)
+  
+  vec <- unlist(vec)
+  
+  return(vec)
+  
+}
+
+
