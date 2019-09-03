d43647148a3b0faa3c63699eefa84b767e07852f
brad_xps <bradley.lindblad@gmail.com>
2019-09-02 20:45:28 -0500

    Added keyring:: to unit tests

--- tests/testthat/test-getquickstat.R
+++ tests/testthat/test-getquickstat.R
@@ -1,6 +1,9 @@
 library(sf)
 library(tigris)
 library(testthat)
+library(keyring)
+
+keyring::key_set("tidyusda")
 
 test_that("getQuickstat1", {
 
@@ -13,7 +16,7 @@ test_that("getQuickstat1", {
     category=NULL,
     domain=NULL,
     county=NULL,
-    key = '7CE0AFAD-EF7B-3761-8B8C-6AF474D6EF71',
+    key = keyring::key_get("tidyusda"),
     program = 'CENSUS',
     data_item = 'CROP TOTALS - OPERATIONS WITH SALES',
     geographic_level = 'COUNTY',
@@ -31,7 +34,7 @@ test_that("getQuickstat2", {
   
   testthat::expect_type(
     
-    getQuickstat(key = '7CE0AFAD-EF7B-3761-8B8C-6AF474D6EF71',
+    getQuickstat(key = keyring::key_get("tidyusda"),
                  program = 'CENSUS',
                  data_item = 'CHICKENS - INVENTORY',
                  geometry = F),
@@ -45,7 +48,7 @@ test_that("getQuickstat3", {
   
   testthat::expect_type(
     
-    getQuickstat(key = '7CE0AFAD-EF7B-3761-8B8C-6AF474D6EF71',
+    getQuickstat(key = keyring::key_get("tidyusda"),
                  program = 'SURVEY',
                  data_item = 'CROP TOTALS, (EXCL HORTICULTURE) - PRODUCTION, MEASURED IN $',
                  geographic_level = 'STATE',
@@ -62,7 +65,7 @@ test_that("getQuickstat4", {
   
   testthat::expect_type(
     
-    getQuickstat(key = '7CE0AFAD-EF7B-3761-8B8C-6AF474D6EF71',
+    getQuickstat(key = keyring::key_get("tidyusda"),
                  program = 'SURVEY',
                  data_item = 'ALMONDS - ACRES BEARING',
                  geographic_level = 'NATIONAL',
