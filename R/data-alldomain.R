3191d6f86f5ff7e78a660eaf77de694a8669dbb4
brad_xps <bradley.lindblad@gmail.com>
2019-09-02 21:42:43 -0500

    Created /data/ files

---
+++ R/data-alldomain.R
@@ -0,0 +1,9 @@
+#' All possible values from the DOMAIN field.
+#'
+#'
+#' @format A vector with 1 variable
+#'
+#' @source \url{https://quickstats.nass.usda.gov}
+"allDomain"
+
+
---
+++ R/data-allprogram.R
@@ -0,0 +1,9 @@
+#' All possible values from the PROGRAM field.
+#'
+#'
+#' @format A vector with 1 variable
+#'
+#' @source \url{https://quickstats.nass.usda.gov}
+"allProgram"
+
+
