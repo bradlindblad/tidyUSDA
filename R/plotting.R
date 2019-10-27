a9deddd9d543637da1475033db048f3101e968d9
BradXPS_Kraken <bradley.lindblad@gmail.com>
2019-10-27 16:28:11 -0500

    Few cosmetic changes and starting to add code coverage

--- R/plotting.R
+++ R/plotting.R
@@ -1,25 +1,30 @@
 #!/usr/bin/R
 # plotting.R
 
+#' plotUSDA
+#' 
+#' @description 
+#' Quickly plot a data frame produced by the getQuickstat() function. 
+#'     
+#' @param df a data frame with a simple feature column (geometry)
+#' 
+#' @param fill_by the value you would like to fill your choropleth output
+#' 
+#' @examples 
+#' \dontrun{
+#' # Use output from getQuickstat()
+#' plotUSDA(df = df_from_getQuickstat)
+#' }
+#' 
+#' @export
 
 plotUSDA <- function(df, fill_by = 'Value'){
   
-  #' plotUSDA
-  #' 
-  #' @description 
-  #' Quickly plot a data frame produced by the getQuickstat() function. 
-  #'     
-  #' @param df a data frame with a simple feature column (geometry)
-  #' 
-  #' @param fill_by the value you would like to fill your choropleth output
-  #' 
-  #' @examples 
-  #' \dontrun{
-  #' # Use output from getQuickstat()
-  #' plotUSDA(df = df_from_getQuickstat)
-  #' }
-  #' 
-  #' @export
+  # Install rgeos if not already installed
+  if (!"rgeos" %in% installed.packages()) {
+    stop("Package \"rgeos\" needed for this function to work. Please install it with install.packages(\"rgeos\")",
+         call. = FALSE)
+  }
 
   
   # Logic to warn if using Mac OsX - issue with plotting
